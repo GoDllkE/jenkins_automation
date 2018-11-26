6e2ea3a057ad094a5a7a2404f3fee640b0bc9122
Gustavo Toledo de Oliveira <gustavo.toledo@compasso.com.br>
2018-11-26 16:17:56 -0200

    JenkinsCore class refactoring. Implemented a new workflow pattern

--- automation/JenkinsCore.py
+++ automation/JenkinsCore.py
@@ -4,16 +4,27 @@
 class JenkinsCore:
 
     def __init__(self, url: str = None, username: str = None, password: str = None):
+        #
         self.url = url
-        self.username = username
-        self.password = password
-        self.bAuth = '{0}:{1}'.format(username, password)
+
+        if username is None:
+            self.username = 'S00000001'
+        else:
+            self.username = username
+
+        if password is None:
+            self.password = 'R0xbV0p7UIjNTFte'
+        else:
+            self.password = password
+
+        # Generate basic-auth
+        self.bAuth = '{0}:{1}'.format(self.username, self.password)
         pass
 
     def get_url(self):
         return self.url
 
-    def get_uer(self):
+    def get_user(self):
         return self.username
 
     def get_passwd(self):
