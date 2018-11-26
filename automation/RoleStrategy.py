54f744addb47c78e1627b15f9fff6fc24f67019f
Gustavo Toledo de Oliveira <gustavo.toledo@compasso.com.br>
2018-11-26 16:19:03 -0200

    Updating ClassCore workflow, now receive JenkinsCore instance instead of standard parameters.

--- automation/RoleStrategy.py
+++ automation/RoleStrategy.py
@@ -1,18 +1,29 @@
-#
-import json
+# Library imports
 import requests
+
+# Internal imports
+from automation.JenkinsCore import JenkinsCore
+
+# Control
+debug = True
 
 
 class RoleStrategy:
 
-    def __init__(self, url: str = None, username: str = None, password: str = None):
+    def __init__(self, jenkins: JenkinsCore):
+        # Retrieve jenkins
+        self.jenkins = jenkins
+
         # Core
-        self.bAuth = '{0}:{1}'.format(username, password)
-        self.bUrl = 'https://{0}@{1}/role-strategy/strategy'.format(self.bAuth, url)
+        self.bUrl = 'http://{0}@{1}/role-strategy/strategy'.format(self.jenkins.get_bAuth(), self.jenkins.get_url())
 
         # Controle de ambientes
         self.env_list = ['dev', 'sit', 'uat', 'prd']
-        pass
+
+        if debug:
+            print("Debugging mode:")
+            print("- Requests send to: {0}".format(self.bUrl))
+            print("- Environments are: {0}".format(str(self.env_list)))
 
     def get_role(self, type: str = None, name: str = None):
         data = dict(roleType=type, roleName=name)
@@ -46,6 +57,12 @@ class RoleStrategy:
         role_permissions = Permissions()
 
         print('Criando role de view')
+        if debug:
+            print("- Name: {0} - View".format(project))
+            print("- Pattern: ^projects(|/{0})".format(project))
+            print("- Permissions: {0}".format(str(role_permissions.view_permissions())))
+            print("- Overwrite: {0}".format(str(True)))
+
         self.create_role(
             type='projectRoles',
             name='{0} - View'.format(project),
@@ -55,6 +72,12 @@ class RoleStrategy:
         )
 
         print('Criando role de build')
+        if debug:
+            print("- Name: {0} - Build".format(project))
+            print("- Pattern: ^projects(|/{0}+(|/build+(.*|/docker(|/.*))))".format(project))
+            print("- Permissions: {0}".format(str(role_permissions.build_permissions())))
+            print("- Overwrite: {0}".format(str(True)))
+
         self.create_role(
             type='projectRoles',
             name='{0} - Build'.format(project),
@@ -64,15 +87,27 @@ class RoleStrategy:
         )
 
         print('Criando role de testes')
+        if debug:
+            print("- Name: {0} - Tests".format(project))
+            print("- Pattern: ^projects(|/{0}+(|/testes(|/.*)))".format(project))
+            print("- Permissions: {0}".format(str(role_permissions.tester_permissions())))
+            print("- Overwrite: {0}".format(str(True)))
+
         self.create_role(
             type='projectRoles',
-            name='{0} - Build'.format(project),
+            name='{0} - Tests'.format(project),
             pattern='^projects(|/{0}+(|/testes(|/.*)))'.format(project),
             perm=role_permissions.tester_permissions(),
             overwrite=True
         )
 
         print('Criando roles de deploy (1/2)')
+        if debug:
+            print("- Name: {0} - Deploy".format(project))
+            print("- Pattern: ^projects(|/{0}+(|/deploy(|/.*)))".format(project))
+            print("- Permissions: {0}".format(str(role_permissions.deploy_permissions())))
+            print("- Overwrite: {0}".format(str(True)))
+
         self.create_role(
             type='projectRoles',
             name='{0} - Deploy'.format(project),
@@ -83,6 +118,12 @@ class RoleStrategy:
 
         print('Criando roles de deploy (2/2)')
         for env in self.env_list:
+            if debug:
+                print("- Name: {0} - {1}".format(project, env))
+                print("- Pattern: ^projects(|/{0}+(|/deploy+(|/{1}(|/.*))))".format(project, env))
+                print("- Permissions: {0}".format(str(role_permissions.deploy_permissions())))
+                print("- Overwrite: {0}".format(str(True)))
+
             self.create_role(
                 type='projectRoles',
                 name='{0} - {1}'.format(project, env),
@@ -96,10 +137,15 @@ class RoleStrategy:
         #
         role_list = ['{0} - {1}'.format(project, item) for item in ['View', 'Build', 'Test', 'Deploy']]
         role_list += ['{0} - Deploy {1}'.format(project, item) for item in self.env_list]
+
         #
-        print("Removendo roles: {0}".format(str(role_list)))
-        self.delete_role(type='projectRoles', name_list=role_list)
-        return 0
+        if debug:
+            print("Debugging mode:")
+            print("- Removendo roles: {0}".format(str(role_list)))
+
+        #
+        response = self.delete_role(type='projectRoles', name_list=role_list)
+        return response.status_code
 
 
 class Permissions:
