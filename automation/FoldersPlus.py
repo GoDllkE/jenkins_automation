3c7e3574cb14959338850deea68e4061069068f1
Gustavo Toledo de Oliveira <gustavo.toledo@compasso.com.br>
2018-11-28 15:47:31 -0200

    Estrutura básica montada para desenvolvimento

--- automation/FoldersPlus.py
+++ automation/FoldersPlus.py
@@ -1,7 +1,22 @@
 #
+from automation import JenkinsCore
 
 
 class FoldersPlus:
 
-    def __init__(self):
+    def __init__(self, jenkins: JenkinsCore, configuration: list = None):
+        """
+        Metodo construtor
+        :param jenkins:         Recebe uma instancia da classe JenkinsCore
+        :param configuration    Recebe uma lista da estrutura padrão a seguir.
+        """
+        self.jenkins = jenkins
+        self.configuration = configuration
+        self.environments = self.jenkins.getEnvironments()
+        self.bUrl = 'https://{0}@{1}'.format(self.jenkins.get_bAuth(), self.jenkins.get_url())
+
+    def create_project_structure(self, project: str = None) -> None:
+        pass
+
+    def delete_project_structure(self, project: str = None) -> None:
         pass
