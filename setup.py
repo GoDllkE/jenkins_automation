70f91d90241947a2d20b454d1a4175f418601780
Gustavo Toledo de Oliveira <gustavo.toledo@compasso.com.br>
2018-12-03 14:53:23 -0200

    Atualizando recursos da automação (role, pattern, directory structure)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='automation',
-    version='3.10.10',
+    version='3.10.10-1',
     packages=["automation"],
     url='https://stash.pontoslivelo.com.br/projects/JNK/repos/automation_role-strategy',
     download_url='https://stash.pontoslivelo.com.br/projects/JNK/repos/automation_role-strategy/archive/master.zip',
