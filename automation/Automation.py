ce5e0e78143dffaeddb94a83eb177b512feff2e0
Gustavo Toledo de Oliveira <gustavo.toledo@compasso.com.br>
2018-12-06 17:20:12 -0200

    Atualizando funções para centralização e cumprimento do proposito

--- automation/Automation.py
+++ automation/Automation.py
@@ -1,19 +1,31 @@
-from automation import RoleStrategy
+import requests
+
+# Importa todos os modulos
+from automation import *
 
 
 class Automation:
-    def __init__(self, role_manager: RoleStrategy = None, configuration: dict = None, debug=False):
+    def __init__(self, jenkins: JenkinsCore = None, role: RoleStrategy = None, config: dict = None, folder: FoldersPlus = None, job: JobManager = None, debug=False):
         """
-            Classe da automacao, responsavel pelas tarefas da automacao.
-            :param debug:
-            :param role_manager:        Recebe uma instancia de uma RoleStrategy
-            :param configuration        Recebe um dicionario de roles da automacao.
+            Construtor
+            :param jenkins:         Recebe uma instancia do jenkins
+            :param role:            Recebe uma instancia das roles
+            :param config:          Recebe uma instancia das configuracoes
+            :param folder:          Recebe uma instancia de estruturas
+            :param job:             Recebe uma instancia de jobs
+            :param debug:           Parametro de definição de debug
         """
+        # Core
         self.debug = debug
-        self.config = configuration
-        self.role_manager = role_manager
+        self.jenkins = jenkins
 
-    def format_perms(self, permissions: list = None) -> str:
+        # Gerenciadores
+        self.job_manager = job
+        self.role_manager = role
+        self.config_manger = config
+        self.folder_manager = folder
+
+    def __format_perms__(self, permissions: list = None) -> str:
         """
             Funcao responsavel pela conversao de dados (compatibilidade)
             :param permissions:     Recebe a lista de permissoes
@@ -21,7 +33,7 @@ class Automation:
         """
         data = ''
         for index, item in enumerate(permissions):
-            if index < (len(permissions)-1):
+            if index < (len(permissions) - 1):
                 data += '{0},'.format(item)
             else:
                 data += '{0}'.format(item)
@@ -39,36 +51,36 @@ class Automation:
         print('Criando role de view')
         self.role_manager.create_role(
             type='projectRoles',
-            name=str(self.config['view_role']['name']).replace('<project>', project),
-            pattern=str(self.config['view_role']['pattern']).replace('<project>', project),
-            perm=str(self.format_perms(self.config['view_role']['permissionsIds'])),
+            name=str(self.config_manger['view_role']['name']).replace('<project>', project),
+            pattern=str(self.config_manger['view_role']['pattern']).replace('<project>', project),
+            perm=str(self.__format_perms__(self.config_manger['view_role']['permissionsIds'])),
             overwrite=True
         )
 
         print('Criando role de build')
         self.role_manager.create_role(
             type='projectRoles',
-            name=str(self.config['build_role']['name']).replace('<project>', project),
-            pattern=str(self.config['build_role']['pattern']).replace('<project>', project),
-            perm=str(self.format_perms(self.config['build_role']['permissionsIds'])),
+            name=str(self.config_manger['build_role']['name']).replace('<project>', project),
+            pattern=str(self.config_manger['build_role']['pattern']).replace('<project>', project),
+            perm=str(self.__format_perms__(self.config_manger['build_role']['permissionsIds'])),
             overwrite=True
         )
 
         print('Criando role de testes')
         self.role_manager.create_role(
             type='projectRoles',
-            name=str(self.config['tests_role']['name']).replace('<project>', project),
-            pattern=str(self.config['tests_role']['pattern']).replace('<project>', project),
-            perm=str(self.format_perms(self.config['tests_role']['permissionsIds'])),
+            name=str(self.config_manger['tests_role']['name']).replace('<project>', project),
+            pattern=str(self.config_manger['tests_role']['pattern']).replace('<project>', project),
+            perm=str(self.__format_perms__(self.config_manger['tests_role']['permissionsIds'])),
             overwrite=True
         )
 
         print('Criando roles de deploy (1/2)')
         self.role_manager.create_role(
             type='projectRoles',
-            name=str(self.config['deploy_role']['name']).replace('<project>', project),
-            pattern=str(self.config['deploy_role']['pattern']).replace('<project>', project),
-            perm=str(self.format_perms(self.config['deploy_role']['permissionsIds'])),
+            name=str(self.config_manger['deploy_role']['name']).replace('<project>', project),
+            pattern=str(self.config_manger['deploy_role']['pattern']).replace('<project>', project),
+            perm=str(self.__format_perms__(self.config_manger['deploy_role']['permissionsIds'])),
             overwrite=True
         )
 
@@ -76,9 +88,10 @@ class Automation:
         for env in self.role_manager.environments:
             self.role_manager.create_role(
                 type='projectRoles',
-                name=str(self.config['deploy_role_env']['name']).replace('<project>', project).replace('<env>', env),
-                pattern=str(self.config['deploy_role_env']['pattern']).replace('<project>', project).replace('<env>', env),
-                perm=str(self.format_perms(self.config['deploy_role_env']['permissionsIds'])),
+                name=str(self.config_manger['deploy_role_env']['name']).replace('<project>', project).replace('<env>', env),
+                pattern=str(self.config_manger['deploy_role_env']['pattern']).replace('<project>', project).replace('<env>',
+                                                                                                             env),
+                perm=str(self.__format_perms__(self.config_manger['deploy_role_env']['permissionsIds'])),
                 overwrite=True
             )
         # End of function
@@ -92,40 +105,73 @@ class Automation:
 
         # Dynamic project roles name list generation (don't blame me)
         role_list = []
-        for item in list(self.config.keys()):
-            if 'env' not in self.config[item]['name']:
-                role_list.append(str(self.config[item]['name']).replace('<project>', project))
+        for item in list(self.config_manger.keys()):
+            if 'env' not in self.config_manger[item]['name']:
+                role_list.append(str(self.config_manger[item]['name']).replace('<project>', project))
             else:
                 for env in self.role_manager.environments:
-                    role_list.append(str(self.config[item]['name']).replace('<project>', project).replace('<env>', env))
+                    role_list.append(str(self.config_manger[item]['name']).replace('<project>', project).replace('<env>', env))
                 continue
             continue
         #
 
         print("Deletando role(s): {0}...".format(str(role_list)), end='')
-        self.role_manager.delete_role(type='projectRoles', name_list=self.format_perms(role_list))
+        self.role_manager.delete_role(type='projectRoles', name_list=self.__format_perms__(role_list))
         print("concluido!")
         # End of function
 
-    def create_role(self, data: dict=None) -> None:
+    def create_deploy_jobs(self, projeto: str = None, repositorio: str = None) -> None:
         """
-            Interface do metodo create da RoleStrategy, que recebe um dict e formata para o metodo da RoleStrategy.
-            :param data:        Recebe um dicionario
-            :return:            Retorna nada
+            Funcao que cria jobs de deploy dado um projeto e um repositorio em especifico
+            :param projeto:                     Nome do projeto a na estrutura do jenkins
+            :param repositorio:                 Nome do repositorio da qual deseja criar os jobs de deploy.
+            :return:                            Retorna nada
         """
-        self.role_manager.create_role(
-            type=data['type'],
-            name=data['name'],
-            pattern=data['pattern'],
-            perm=str(self.format_perms(self.config['view_role']['permissionsIds'])),
-            overwrite=data['overwrite']
-        )
+        # Core
+        header = {"Content-Type": "text/xml"}
+
+        # Carrega configurações de job
+        global_config = Configurator()
+        xml = global_config.load_job_config()
+
+        # Controle
+        name = repositorio.split('/')[-1].split('.', 1)[0]
+        path = "{0}/job/{1}/job/deploy/job/<env>/createItem?name={2}".format(self.jenkins.getBUrl(), projeto, name)
+
+        #
+        for env in self.jenkins.getEnvironments():
+            print("Criando job de deploy {0} para {1}...".format(name, env), end='')
+            response = requests.post(url=path.replace('<env>', env), headers=header, data=xml)
+            if response.status_code in [200, 201]:
+                print('concluido com sucesso!')
+            else:
+                print("falhou...(codigo: {0})".format(response.status_code))
+            pass
         pass
 
-    def delete_role(self, data: dict=None) -> None:
-        """
-            Interface do metodo delete da RoleStrategy, que recebe um dit e formata para o metodo da RoleStrategy
-            :param data:        Recebe um dicionario
-            :return:            Retorna nada
-        """
-        self.role_manager.delete_role(type=data['type'], name_list=data['name'])
+    # ================================================================================================================ #
+    #                                     Funcoes de interfaciamento                                                   #
+    # ================================================================================================================ #
+
+    # def create_role(self, data: dict = None) -> None:
+    #     """
+    #         Interface do metodo create da RoleStrategy, que recebe um dict e formata para o metodo da RoleStrategy.
+    #         :param data:        Recebe um dicionario
+    #         :return:            Retorna nada
+    #     """
+    #     self.role_manager.create_role(
+    #         type=data['type'],
+    #         name=data['name'],
+    #         pattern=data['pattern'],
+    #         perm=str(self.__format_perms(self.config['view_role']['permissionsIds'])),
+    #         overwrite=data['overwrite']
+    #     )
+    #     pass
+    #
+    # def delete_role(self, data: dict = None) -> None:
+    #     """
+    #         Interface do metodo delete da RoleStrategy, que recebe um dit e formata para o metodo da RoleStrategy
+    #         :param data:        Recebe um dicionario
+    #         :return:            Retorna nada
+    #     """
+    #     self.role_manager.delete_role(type=data['type'], name_list=data['name'])
