976bd7db945ea017dc5e1b4f5f875fce80d36755
Gustavo Toledo de Oliveira <gustavo.toledo@compasso.com.br>
2018-12-14 19:33:41 -0200

    Corrigindo mensagem de debug no inicializador

--- automation/__init__.py
+++ automation/__init__.py
@@ -90,11 +90,11 @@ def automate():
     #
     if debug:
         print("Debug: enabled.")
-        if action['id']:
+        if action.get('id'):
             dado = action['id']
         else:
             dado = action['name']
-        print("- Action: {0} {1} {2}\n\n".format(action['acao'], action['dado'],dado))
+        print("- Action: {0} {1} {2}\n\n".format(action['acao'], action['dado'], dado))
 
     # Cria instancias
     jnk = JenkinsCore()
