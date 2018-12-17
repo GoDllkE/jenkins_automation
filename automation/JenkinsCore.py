d9b2344300a4ccdd08e96d52cccde5c4bd342673
Gustavo Toledo de Oliveira <gustavo.toledo@compasso.com.br>
2018-12-17 13:14:33 -0200

    Adicionado docstring das funcionalidades

--- automation/JenkinsCore.py
+++ automation/JenkinsCore.py
@@ -25,21 +25,45 @@ class JenkinsCore:
     # =============================================================================================== #
 
     def get_url(self) -> str:
+        """
+            Funcao GET da url padrao do jenkins
+            :return:    retorna URL
+        """
         return self.url
 
     def get_user(self) -> str:
+        """
+            Funcao GET do usuario BOT do jenkins
+            :return:    retorna nome do usuario
+        """
         return self.username
 
     def get_passwd(self) -> str:
+        """
+            funcao GET da senha do usuario BOT
+            :return:    retorna senha do usuario
+        """
         return self.password
 
     def get_bauth(self) -> str:
+        """
+            funcao GET da formatacao HTTP_BASIC_AUTH com usuario BOT
+            :return:    retorna HTTP_BASIC_AUTH do usuario
+        """
         return self.b_auth
 
     def get_environments(self) -> list:
+        """
+            funcao GET da lista de ambientes disponiveis no jenkins
+            :return:    retorna lista de ambientes
+        """
         return self.environment
 
     def get_burl(self) -> list:
+        """
+            funcao GET da url do jenkins contendo uma HTTP_BASIC_AUTH
+            :return:    retorna url formatada para requisicoes http via biblioteca requests
+        """
         return "http://{0}@{1}".format(self.get_bauth(), self.get_url())
 
     # =============================================================================================== #
